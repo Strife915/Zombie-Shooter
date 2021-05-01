273f2607dae64816ca70612ad63fc0afc0be9dc6
Strife915 <thekarabulut95@gmail.com>
2021-05-01 18:43:06 +0300

    2

---
+++ Zombie Shot/Assets/Scripts/DeathHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathHandler : MonoBehaviour
+{
+    [SerializeField] Canvas gamerOverCanvas;
+
+    private void Start()
+    {
+        gamerOverCanvas.enabled = false;
+    }
+    public void HandleDeath()
+    {
+        gamerOverCanvas.enabled = true;
+        Time.timeScale = 0;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
---
+++ Zombie Shot/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+    
+
+
+    public void Takedamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints <= 0)
+        {
+            GetComponent<DeathHandler>().HandleDeath();
+            Debug.Log("Player is dead");
+        }
+    }
+}
